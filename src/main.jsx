4f21e090504afb0d869fb1d2c6df6296b05c5696
HarshForreal <hp673315@gmail.com>
2024-06-30 12:06:18 +0530

    added Navbar route

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,35 @@
-// import ReactDOM from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
-import "./index.css";
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Layout from "./Layout.jsx"; // Import the layout component
 import Contact from "./pages/Contact.jsx";
 import About from "./pages/AboutPage.jsx";
+import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
-  },
-  {
-    path: "/About",
-    element: <About />,
+    element: <Layout />, // Use the layout component
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
   },
 ]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-    {/* <App /> */}
   </React.StrictMode>
 );
