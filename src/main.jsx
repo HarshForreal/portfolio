4ec54ef9b375197d0102e95789ff15ab931491df
HarshForreal <hp673315@gmail.com>
2024-07-15 14:39:10 +0530

    modified main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -31,6 +31,10 @@ const router = createBrowserRouter([
       },
     ],
   },
+  {
+    path: "/about",
+    element: <About />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
